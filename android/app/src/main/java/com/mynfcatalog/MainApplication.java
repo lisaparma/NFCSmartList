3b76cb96a602797ed42948d4a3043cc3fcd6e16f
Lisa <lisa.parma@hotmail.it>
2018-12-29 15:06:56 +0100

    Font Android

--- android/app/src/main/java/com/mynfcatalog/MainApplication.java
+++ android/app/src/main/java/com/mynfcatalog/MainApplication.java
@@ -37,10 +37,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new NfcManagerPackage(),
-            new RNGestureHandlerPackage(),
-            new RNFirebasePackage(),
           new NfcManagerPackage(),
           new VectorIconsPackage(),
           new RNGestureHandlerPackage(),
